501e4d64a0422bcf46a5dfe779611e1faa117487
dhaya <dhayabharanmj@gmail.com>
2026-04-08 16:42:14 +0530

    added static root for deployment

--- hill_construction_dss/hill_dss/settings.py
+++ hill_construction_dss/hill_dss/settings.py
@@ -1,13 +1,45 @@
 from pathlib import Path
+import os   # 👈 ADD THIS
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 SECRET_KEY = 'django-insecure-hill-dss-v3-change-in-production'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
-INSTALLED_APPS = ['django.contrib.staticfiles', 'rest_framework', 'dss']
-MIDDLEWARE = ['django.middleware.security.SecurityMiddleware', 'django.middleware.common.CommonMiddleware']
+
+INSTALLED_APPS = [
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'dss'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.common.CommonMiddleware'
+]
+
 ROOT_URLCONF = 'hill_dss.urls'
-TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates', 'DIRS': [], 'APP_DIRS': True, 'OPTIONS': {'context_processors': ['django.template.context_processors.request']}}]
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.request'
+        ]
+    }
+}]
+
 WSGI_APPLICATION = 'hill_dss.wsgi.application'
+
+# ✅ STATIC SETTINGS
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 👈 ADD THIS
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-REST_FRAMEWORK = {'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'], 'DEFAULT_PARSER_CLASSES': ['rest_framework.parsers.JSONParser']}
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': ['rest_framework.renderers.JSONRenderer'],
+    'DEFAULT_PARSER_CLASSES': ['rest_framework.parsers.JSONParser']
+}
